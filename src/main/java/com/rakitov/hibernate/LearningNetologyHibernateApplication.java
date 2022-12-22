14ec2d7189ee110d007999ee437698ce3f5e8220
Stanislav Rakitov <prosperousrf@gmail.com>
2022-12-22 20:22:50 +0300

    Initial commit

---
+++ src/main/java/com/rakitov/hibernate/LearningNetologyHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.rakitov.hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningNetologyHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningNetologyHibernateApplication.class, args);
+    }
+
+}
